15f07e58797accb0a13627aa02e268fda7810e07
Venketesh Pratap Mall <venketeshmall2@gmail.com>
2023-11-30 11:51:00 +0000

    etrdetrd

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://venketeshmall2:qqQd08kHG79LKtiy@cluster0.czrohkn.mongodb.net/?retryWrites=true&w=majority';
+// Use environment variables or a configuration file to store sensitive information
+const uri = 'mongodb+srv://venketeshmall2:7p9T3Eghx0kpoLOL@cluster0.zv3ejx9.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(uri).then(()=>{
-     console.log('Connected to Database: MongoDB Atlas');
- }).catch((err) => console.log("no connection " + err));
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to Database: MongoDB Atlas');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB Atlas:', err.message);
+  });
 
 const db = mongoose.connection;
-module.exports = db
+module.exports = db;
